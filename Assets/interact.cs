fda54ea61b3542d599a5c922326c729da35e3b6d
charliesunliu <cccharliesunliu@gmail.com>
2016-12-03 15:39:24 -0800

    hisTour_Project

---
+++ Assets/interact.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MyMenu : MonoBehaviour
+{
+	public GameObject myMenu; // set this in the inspector
+
+	public void ToggleMenu()
+	{
+		myMenu.SetActive(!myMenu.activeInHierarchy);
+	}
+}
